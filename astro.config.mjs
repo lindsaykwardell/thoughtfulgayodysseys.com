41622f68499c69cc1867a91903e73f098e2bc972
Lindsay Wardell <three060@gmail.com>
2024-06-12 10:51:49 -0700

    The hills are alive with the sound of Storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,40 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     drafts: true,
     shikiConfig: {
-      theme: "css-variables"
-    }
+      theme: "css-variables",
+    },
   },
   shikiConfig: {
     wrap: true,
     skipInline: false,
-    drafts: true
+    drafts: true,
   },
-  site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap(), mdx()]
+  site: "https://lexingtonthemes.com",
+  integrations: [
+    tailwind(),
+    sitemap(),
+    mdx(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+        post: "storyblok/Post",
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: "us", // optional,  or 'eu' (default)
+      },
+    }),
+  ],
 });
