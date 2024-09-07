2cea4e0ea2ca39ad007852c04ad324af0fb4c908
Lindsay Wardell <three060@gmail.com>
2024-09-07 11:22:56 -0700

    Lots of changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     screens: {
       sm: "540px",
@@ -94,29 +94,29 @@ module.exports = {
     extend: {
       colors: {
         blue: {
-         50: "#EBEBFF",
-      100: "#D2D2FE",
-      200: "#A6A4FE",
-      300: "#7E7CFD",
-      400: "#524FFD",
-      500: "#2522FC",
-      600: "#0703E2",
-      700: "#0502AB",
-      800: "#03026F",
-      900: "#020137",
-      950: "#01001E"
+          50: "#EBEBFF",
+          100: "#D2D2FE",
+          200: "#A6A4FE",
+          300: "#7E7CFD",
+          400: "#524FFD",
+          500: "#2522FC",
+          600: "#0703E2",
+          700: "#0502AB",
+          800: "#03026F",
+          900: "#020137",
+          950: "#01001E",
         },
       },
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        display: ["Gilda Display", ...defaultTheme.fontFamily.sans],
+        sans: ["Raleway Variable", ...defaultTheme.fontFamily.sans],
+        display: ["Cormorant Garamond", "serif"],
         mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
       },
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
   ],
-}
+};
