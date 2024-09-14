57307721c1236c093b6020ae07945b903a02206f
Lindsay Wardell <three060@gmail.com>
2024-09-13 21:07:38 -0700

    Start work on RSS feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,63 @@
+import rss from "@astrojs/rss";
+import { ISbRichtext } from "@storyblok/astro";
+import { useStoryblokApi } from "@storyblok/astro";
+
+type Post = {
+  name: string;
+  created_at: string;
+  published_at: string;
+  id: number;
+  uuid: string;
+  content: {
+    _uid: string;
+    tags: string[];
+    content: ISbRichtext;
+    summary: string;
+    component: "post";
+    sideImage: {
+      id: number;
+      filename: string;
+    };
+    coverImage: {
+      id: number;
+      filename: string;
+    };
+  };
+  slug: string;
+  full_slug: string;
+};
+
+export async function GET() {
+  const storyblokApi = useStoryblokApi();
+
+  console.log(storyblokApi);
+
+  const {
+    data,
+  }: {
+    data: {
+      stories: Post[];
+    };
+  } = await storyblokApi.get("cdn/stories", {
+    version: import.meta.env.STORYBLOK_MODE,
+  });
+
+  const posts = data.stories.toSorted(
+    (a, b) =>
+      new Date(b.published_at ?? b.created_at).getTime() -
+      new Date(a.published_at ?? a.created_at).getTime()
+  );
+
+  return rss({
+    title: "Thoughtful Gay Odysseys",
+    description: "Poetry by Rebekah Wardell",
+    site: "https://thoughtfulgayodysseys.com",
+    items: posts.map((post) => ({
+      title: post.name,
+      pubDate: new Date(post.published_at ?? post.created_at),
+      description: post.content.summary,
+      link: `/posts/${post.slug}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
